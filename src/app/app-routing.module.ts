0f1d5b6f9868acc2b2d9c17b5d05470e0cdcbd59
jmhs11 <josemariahs@gmail.com>
2022-03-20 20:36:48 +0100

    Add todos and wildcard to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './pages/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
