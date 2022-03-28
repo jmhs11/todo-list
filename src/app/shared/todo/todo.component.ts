5fb320447cda4937aee721f01483206859c736a5
jmhs11 <josemariahs@gmail.com>
2022-03-28 22:18:35 +0200

    Update todo component

--- src/app/shared/todo/todo.component.ts
+++ src/app/shared/todo/todo.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
 })
 export class TodoComponent implements OnInit {
+  @Input() todo!: Todo;
+
+  @Output() onEditTodo = new EventEmitter<Todo>();
+  @Output() onToggleDone = new EventEmitter<Todo>();
+  @Output() onDeleteTodo = new EventEmitter<number>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  editTodo(todo: Todo) {
+    this.onEditTodo.emit(todo);
+  }
+
+  toggleDone(todo: Todo) {
+    this.onToggleDone.emit({ ...todo, done: !todo.done });
+  }
+
+  deleteTodo(todoId: number) {
+    this.onDeleteTodo.emit(todoId);
+  }
 }
