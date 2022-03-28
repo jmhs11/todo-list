f53c3475e6ca5218fabc5ce1694b14c821fe7a96
jmhs11 <josemariahs@gmail.com>
2022-03-28 22:21:06 +0200

    Update todo list

--- src/app/pages/todos/fragments/todo-list/todo-list.component.ts
+++ src/app/pages/todos/fragments/todo-list/todo-list.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/shared/todo/models/todo.model';
+import { TodoService } from 'src/app/shared/todo/services/todo.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styles: [
-  ]
 })
 export class TodoListComponent implements OnInit {
+  @Output() editTodo = new EventEmitter<Todo>();
 
-  constructor() { }
+  constructor(readonly todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe({
+      next: (todos) => {},
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 
+  onEditTodo(todo: Todo) {
+    this.editTodo.emit(todo);
+  }
+
+  onToggleDone(todo: Todo) {
+    this.todoService.updateTodo(todo.id, todo).subscribe({
+      next: (todo) => {},
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  onDeleteTodo(todoId: number) {
+    this.todoService.deleteTodo(todoId).subscribe({
+      next: () => {
+        console.log('todo deleted');
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
