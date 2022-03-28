3e2d1568410ebfa481fde2cb0991f0c26bcd3299
jmhs11 <josemariahs@gmail.com>
2022-03-28 21:48:47 +0200

    Extend tailwind clases

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        form: "repeat(3, 1fr)",
+      },
+      gridTemplateColumns: {
+        form: "repeat(2, 1fr)",
+        todo: "5% 35% 40% 10% 10%",
+      },
+    },
   },
   plugins: [],
 };
