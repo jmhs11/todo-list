b7b918a6e626d1ed27b64342252c2265272fc936
jmhs11 <josemariahs@gmail.com>
2022-03-28 22:23:12 +0200

    Update todos page

--- src/app/pages/todos/todos.component.ts
+++ src/app/pages/todos/todos.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/shared/todo/models/todo.model';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[] = [];
+  editTodo!: Todo;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onEditTodo(todo: Todo) {
+    this.editTodo = todo;
+  }
 }
