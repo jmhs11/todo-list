70ad4e4ddcc5c3a41e67d8a22b0703437c147511
jmhs11 <josemariahs@gmail.com>
2022-03-28 22:20:22 +0200

    Update todo form

--- src/app/pages/todos/fragments/todo-form/todo-form.component.ts
+++ src/app/pages/todos/fragments/todo-form/todo-form.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/app/shared/todo/models/todo.model';
+import { TodoService } from 'src/app/shared/todo/services/todo.service';
 
 @Component({
   selector: 'app-todo-form',
   templateUrl: './todo-form.component.html',
-  styles: [
-  ]
 })
 export class TodoFormComponent implements OnInit {
+  todoForm: FormGroup = this.fb.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+  });
 
-  constructor() { }
+  @Output() onSubmitForm = new EventEmitter<Todo>();
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private todoService: TodoService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: FormGroup) {
+    const { title, description } = form.value;
+    const todo: Todo = {
+      id: Date.now(),
+      title,
+      description,
+      done: false,
+    };
+
+    this.todoService.addTodo(todo).subscribe({
+      next: (todo) => {
+        this.todoForm.reset();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
-
 }
