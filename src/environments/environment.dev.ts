16aebd245c31879f0eca5128247c04e66e40f8bd
jmhs11 <josemariahs@gmail.com>
2022-04-05 18:53:55 +0200

    Update github action

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCllhUx351B5r6eaLSgNWHQxDqKarnx874',
+    authDomain: 'todo-list-31aff.firebaseapp.com',
+    projectId: 'todo-list-31aff',
+    storageBucket: 'todo-list-31aff.appspot.com',
+    messagingSenderId: '626552814559',
+    appId: '1:626552814559:web:5719abaf6f46e4e533226a'
+  },
+  appVersion: '0.0.0',
+  production: 'false',
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  firebase: {
+    apiKey: 'API_KEY',
+    authDomain: 'AUTH_DOMAIN',
+    projectId: 'PROJECT_ID',
+    storageBucket: 'STORAGE_BUCKET',
+    messagingSenderId: 'MESSAGING_SENDER_ID',
+    appId: 'APP_ID',
+  },
+  appVersion: '0.0.0',
+  production: true,
 };
