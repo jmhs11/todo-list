034ea6392ae890f388d4feff855685d9f35d82b7
jmhs11 <josemariahs@gmail.com>
2022-04-09 16:39:12 +0200

    Change default page to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { TodosComponent } from './pages/todos/todos.component';
 
 const routes: Routes = [
-  { path: '', component: TodosComponent, pathMatch: 'full' },
+  { path: '', component: LoginComponent, pathMatch: 'full' },
+  { path: 'todos', component: TodosComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '' },
 ];
 
