60b9df0fdca721fd75de0af47d27d71e088a254e
jmhs11 <josemariahs@gmail.com>
2022-04-10 21:14:30 +0200

    Update header and login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/auth/auth.service';
 
 @Component({
@@ -6,11 +8,35 @@ import { AuthService } from 'src/app/shared/auth/auth.service';
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  constructor(private authService: AuthService) {}
+  emailRegexp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(this.emailRegexp)]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
+  loginEmail() {
+    this.authService.emailLogin(this.email?.value, this.password?.value);
+  }
+
+  registerEmail() {
+    this.authService.registerEmail(this.email?.value, this.password?.value);
+  }
 
   loginWithGoogle() {
     this.authService.googleLogin();
   }
+
   loginWithGithub() {
     this.authService.githubLogin();
   }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { RouterLink, RouterModule } from '@angular/router';
-import { AuthService } from 'src/app/shared/auth/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, AngularSvgIconModule],
 })
 export class LoginModule {}
